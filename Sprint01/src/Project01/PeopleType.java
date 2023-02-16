a40fe916e091208fd4b0c5d153eae17197f5ba91
braden <kirkpatrickb02@moravian.edu>
2023-02-16 10:24:34 -0500

    documented PeopleType

--- Sprint01/src/Project01/PeopleType.java
+++ Sprint01/src/Project01/PeopleType.java
@@ -1,5 +1,9 @@
 package Project01;
-
+/*
+ * PeopleType is an enum for the types that players have
+ * each type has an discription that is associated with it 
+ * and can return that discription with getDescription()
+ */
 public enum PeopleType
 {
     warrior ("Solder"),
@@ -7,13 +11,18 @@ public enum PeopleType
     healer ("Mercy");
 
     private String description;
-
+    
+    /*
+     * constructs a enum with a discription 
+     */
     PeopleType (String types)
     {
         description = types;
     }
 
-
+    /*
+     * @return String discription
+     */
     public String getDescription()
     {
         return description;
