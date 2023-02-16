6709f6f408b13235397f517f3b5f4f1da39bf7fb
badlydrawnface <bdface@proton.me>
2023-02-16 10:33:39 -0500

    added javadoc in People

--- Sprint01/src/Project01/People.java
+++ Sprint01/src/Project01/People.java
@@ -4,6 +4,10 @@ import Project01.PeopleType;
 
 public class People
 {
+  /**
+   * The People class holds the information that a person within the game, will have; This includes the 
+   * person's name, associated nation, associated tribe, its type, a description, and its lifepoints.
+   */
     private String personName;
     private String myNation;
     private String myTribe;
@@ -21,31 +25,56 @@ public class People
         myLifePoints = lifePoints;
     }
 
+    /**
+     * Returns the type of the person
+     * @return PeopleType
+     */
     public PeopleType getType()
     {
         return me;
     }
 
+    /**
+     * Returns the tribe of the person
+     * @return tribe
+     */
     public String getTribe()
     {
         return myTribe;
     }
 
+    /**
+     * Returns the person's nation
+     * @return nation
+     */
     public String getNation()
     {
         return myNation;
     }
 
+    /**
+     * Return whether or not the Person is alive
+     * @return true/false
+     */
     public Boolean isPersonAlive()
     {
         return (myLifePoints > 0);
     }
 
+    /**
+     * Returns the lifepoints the person has (left)
+     * @return lifepoints
+     */
     public int getLifePoints()
     {
         return myLifePoints;
     }
 
+    /**
+     * Provides the scenarios when another provided person (parameter) is (or is not) from another nation, as well as scenarios
+     * based on the types of the people.
+     * @param People otherPerson
+     */
     public void encounterStrategy(People otherPerson)
     {
         if(myNation.equals(otherPerson.getNation()))
@@ -62,12 +91,19 @@ public class People
     }
 
 
+    /**
+     * Reduces the amount of lifepoints a person has by an amount provided by the points parameter
+     * @param int points
+     */
     public void reduceLifePoints(int points)
     {
         myLifePoints = myLifePoints - points;
     }
 
-
+    /**
+     * Concatenates the nation name, tribe name, person name, description, and remaining lifepoints into a string with 
+     * a tabbed format 
+     */
     public String toString()
     {
         String result = new String( myNation + "\t" +  myTribe + "\t" + me + "\t" + myDescription + "\t" + myLifePoints);
